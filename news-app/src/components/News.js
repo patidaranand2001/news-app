c6df54c3a5fc595280a57a953ec5c2bc6dc9b1b7
Anand <anandpatidar603@gmail.com>
2023-11-30 13:47:40 +0530

    cards are aligned in a row

--- news-app/src/components/News.js
+++ news-app/src/components/News.js
@@ -2,7 +2,58 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export default class News extends Component {
+articles =[
+  {
 
+     
+    "source": {
+        "id": null,
+        "name": "Zacjohnson.com"
+    },
+    "author": "Zac Johnson",
+    "title": "Dana Perino Net Worth – How Much is Perino Worth?",
+    "description": "Dana Perino, the former White House Press Secretary, has amassed a significant net worth throughout her career. Known for her role as a co anchor on FOX News Channel’s morning news program, America’s Newsroom, Perino has established herself as a prominent fig…",
+    "url": "https://zacjohnson.com/dana perino net worth/",
+    "urlToImage": "https://zacjohnson.com/wp content/uploads/2023/11/dana perino net worth.jpg",
+    "publishedAt": "2023 11 29T06:32:18Z",
+    "content": "Dana Perino, the former White House Press Secretary, has amassed a significant net worth throughout her career. Known for her role as a co anchor on FOX News Channel’s morning news program, America’s… [+19743 chars]"
+
+},
+ 
+{
+
+     
+    "source": {
+        "id": null,
+        "name": "Investing.com"
+    },
+    "author": "Investing.com",
+    "title": "Pro Research: Wall Street digs into SolarEdge's future",
+    "description": "Pro Research: Wall Street digs into SolarEdge's future",
+    "url": "https://www.investing.com/news/stock market news/pro research wall street digs into solaredges future 93CH 3245294",
+    "urlToImage": "https://i invdn com.investing.com/news/WallStreet_800x533_L_1611376827.jpg",
+    "publishedAt": "2023 11 29T06:17:16Z",
+    "content": "Explore Wall Street's expert insights with this ProResearch article, which will exclusively be available to InvestingPro subscribers soon. Enhance your investment strategy with ProPicks, our newest p… [+10449 chars]"
+
+},
+ 
+{
+
+     
+    "source": {
+        "id": null,
+        "name": "Biztoc.com"
+    },
+    "author": "reuters.com",
+    "title": "Taiwanese Tesla supplier Hota retools supply chain with first US factory",
+    "description": "[1/7] Hota's CEO Holly Sheng poses for a photo in their factory in Taichung, Taiwan November 13, 2023. REUTERS/Ann Wang Acquire Licensing Rights TAICHUNG, Taiwan, Nov 29 (Reuters)   For years, Hota Industrial Mfg. Co has made gears, shafts and other auto part…",
+    "url": "https://biztoc.com/x/5e83256afd9c4aef",
+    "urlToImage": "https://c.biztoc.com/p/5e83256afd9c4aef/s.webp",
+    "publishedAt": "2023 11 29T06:14:09Z",
+    "content": "[1/7] Hota's CEO Holly Sheng poses for a photo in their factory in Taichung, Taiwan November 13, 2023. REUTERS/Ann Wang Acquire Licensing RightsTAICHUNG, Taiwan, Nov 29 (Reuters)   For years, Hota In… [+292 chars]"
+
+}
+]
 constructor(){
   super();
   console.log("hello i am aconstructor from  news component");
@@ -11,28 +62,30 @@ constructor(){
     loading:false
   }
 }
-async componentDidMount(){
-let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=6fd58bc66dea433582af83bbf68f3ef7";
-let data=await fetch(url);
-let parsedData=data.json()
-console.log(parsedData)
-this.setState({articles:parsedData.articles})
-}
+// async componentDidMount(){
+// let url="https://newsapi.org/v2/top headlines?country=in&apiKey=6fd58bc66dea433582af83bbf68f3ef7";
+// let data=await fetch(url);
+// let parsedData=data.json()
+// console.log(parsedData)
+// this.setState({articles:parsedData.articles})
+// }
 
   render() {
-    return (
-     <div className='container my-4 '>
-      <h2>NewsMonkey - Top headlines</h2>
-       <div className='row'>
-        {
-          this.state.articles.map((element)=>{
-             return <div className='col-md-4' key={element.url}>
-             <NewsItem title={element.title} desc={element.description} imgurl={element.urlToImage}/>
-                  </div>
-          })
-        }
-       </div>
-     </div>
+    return(
+     
+      <div className='container '>
+        <h2>NewsMonkey  Top headlines</h2>
+        <div className='row'>
+        {this.state.articles.map((element)=>{
+          return <div   className='col-md-3 mx-3' key={element.url}>
+            
+             <NewsItem  title={element.title.slice(0,50)} desc={element.description.slice(0,80)} imgurl={element.urlToImage} newsUrl={element.url}/>
+            
+            </div>
+        })}
+        </div>
+
+      </div>
     )
   }
 }
--- news-app/src/components/NewsItem.js
+++ news-app/src/components/NewsItem.js
@@ -2,15 +2,16 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title,desc,imgurl}=this.props;
+    let {title,desc,imgurl,newsUrl}=this.props;
     return (
-      <div>
-   <div  className="card my-3" style={{width: "18rem"}}>
+      <div className='my-3 '>
+   <div  className="card " style={{width: "18rem"}}>
   <img src={imgurl}  className="card-img-top" alt="..."/>
   <div  className="card-body">
-    <h5  className="card-title">{title}</h5>
-    <p  className="card-text">{desc}</p>
-    <a href="#"  className="btn btn-sm btn-primary">Go somewhere</a>
+    <h5  className="card-title">{title}..</h5>
+    <p  className="card-text">{desc}..</p>
+    <a href= {newsUrl} target='
+    _blank' className="btn btn-sm btn-primary" >Read more</a>
   </div>
 </div>
 
