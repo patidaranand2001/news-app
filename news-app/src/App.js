d16bbe0ceb7aefd777a2e8ef15b0cd8eae24a4b3
Anand <anandpatidar603@gmail.com>
2023-12-02 23:23:19 +0530

    page size updated

--- news-app/src/App.js
+++ news-app/src/App.js
@@ -8,18 +8,19 @@ import { Routes } from 'react-router-dom';
 
 
 export default class App extends Component {
+  pageSize=15;
   render() {
     return (
       <div>
         <NavBar />
 
         <Routes>
-          <Route exect path="/" element={<News key='general' pagesize={6} country='us' category='general' />} />
-          <Route exect path="/business" element={<News key='business' pagesize={6} country='us' category='business' />} />
-          <Route exect path="/entertainment" element={<News key='entetainment' pagesize={6} country='us' category='entertainment' />} />
-          <Route exect path="/sports" element={<News key='sports' pagesize={6} country='us' category='sports' />} />
-          <Route exect path="/science" element={<News key='science' pagesize={6} country='us' category='science' />} />
-          <Route exect path="/technology" element={<News key='technology' pagesize={6} country='us' category='technology' />} />
+          <Route exect path="/" element={<News key='general' pagesize={this.pageSize} country='us' category='general' />} />
+          <Route exect path="/business" element={<News key='business' pagesize={this.pageSize} country='us' category='business' />} />
+          <Route exect path="/entertainment" element={<News key='entetainment' pagesize={this.pageSize} country='us' category='entertainment' />} />
+          <Route exect path="/sports" element={<News key='sports' pagesize={this.pageSize} country='us' category='sports' />} />
+          <Route exect path="/science" element={<News key='science' pagesize={this.pageSize} country='us' category='science' />} />
+          <Route exect path="/technology" element={<News key='technology' pagesize={this.pageSize} country='us' category='technology' />} />
 
         </Routes>
       </div>
